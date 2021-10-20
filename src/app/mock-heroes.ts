bd824c9ce3aacb5eb1a144304328b5b25d8d1e96
Arthur Yeganyan <arthur.yeganyan@synisys.com>
2021-10-20 18:45:18 +0400

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from "./heroes/hero";
+
+export const HEROES: Hero[] = [
+  { id: 11, name: "Luke Skywalker" },
+  { id: 12, name: "Darth Vader" },
+  { id: 13, name: "Han Solo" },
+  { id: 14, name: "Lea Organa" },
+  { id: 15, name: "Darth Sidious" }
+]
